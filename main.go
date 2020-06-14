2fe773ff5b411a3acdcbac7ac806a78df0f51f06
PawelPleskaczynski <pawelpleskaczynski@gmail.com>
2020-06-14 14:23:52 +0200

    added RGB combination, histogram stretching, fixed some bugs

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"image/png"
 	"io"
 	"log"
+	"math"
 	"os"
 )
 
@@ -21,14 +22,16 @@ func check(e error) {
 	}
 }
 
-func saveImage(channel, width, height int, data []uint16) {
+func processFrame(channel, width, height int, data []uint16, stretch, southbound bool) *image.Gray16 {
 	log.Printf("Processing channel %d...", channel)
 	data = data[750:]
 
 	j, count10240, count850 := 0, 0, 0
 	array512 := make([]uint16, len(data))
 
-	for i := channel * 2; i < len(data); i++ {
+	var maxVal uint16
+
+	for i := channel; i < len(data); i++ {
 		if count10240 < 10240 {
 			array512[j] = data[i]
 			j++
@@ -43,23 +46,74 @@ func saveImage(channel, width, height int, data []uint16) {
 	}
 	data = array512
 
-	image := image.NewGray16(image.Rect(0, 0, width, height))
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			image.SetGray16(x, y, color.Gray16{data[y*width+x]})
+	if stretch {
+		for i := 0; i < len(array512); i++ {
+			if array512[i] > maxVal {
+				maxVal = array512[i]
+			}
 		}
 	}
+
+	frame := image.NewGray16(image.Rect(0, 0, width, height))
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if southbound {
+				if stretch {
+					frame.SetGray16(width-x-1, height-y-1, color.Gray16{uint16(float64(data[y*width+x]) / (float64(maxVal) / float64(math.MaxUint16)))})
+				} else {
+					frame.SetGray16(width-x-1, height-y-1, color.Gray16{data[y*width+x]})
+				}
+			} else {
+				if stretch {
+					frame.SetGray16(x, y, color.Gray16{uint16(float64(data[y*width+x]) / (float64(maxVal) / float64(math.MaxUint16)))})
+				} else {
+					frame.SetGray16(x, y, color.Gray16{data[y*width+x]})
+				}
+			}
+		}
+	}
+
+	return frame
+}
+
+func saveImageRGB(frameR, frameG, frameB *image.Gray16, ch1, ch2, ch3 int) {
+	out, err := os.Create(fmt.Sprintf("%s_%d%d%d.png", (*filename), ch1, ch2, ch3))
+	check(err)
+	defer out.Close()
+	frame := image.NewRGBA64(image.Rect(0, 0, width, height))
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			frame.SetRGBA64(x, y, color.RGBA64{frameR.Gray16At(x, y).Y, frameG.Gray16At(x, y).Y, frameB.Gray16At(x, y).Y, math.MaxUint16})
+		}
+	}
+	png.Encode(out, frame)
+	log.Printf("Saved RGB %d%d%d image", ch1, ch2, ch3)
+}
+
+func saveImageGray(frame *image.Gray16, channel int) {
 	out, err := os.Create(fmt.Sprintf("%s_channel_%d.png", (*filename), channel))
 	check(err)
 	defer out.Close()
-	png.Encode(out, image)
+	png.Encode(out, frame)
 	log.Printf("Saved channel %d", channel)
 }
 
 func main() {
 	filename = flag.String("input", "", "input file")
 	channel := flag.Int("channel", -1, "channel of the image, -1 returns all the channels")
+	stretch := flag.Bool("stretch", false, "stretch histogram")
+	southbound := flag.Bool("south", false, "enable this option for southbound passes")
+	rChannel := flag.Int("r", -1, "image channel to be represented by red channel (for NOAA images ranges from 0-4)")
+	gChannel := flag.Int("g", -1, "image channel to be represented by green channel (for NOAA images ranges from 0-4)")
+	bChannel := flag.Int("b", -1, "image channel to be represented by blue channel (for NOAA images ranges from 0-4)")
 	flag.Parse()
+
+	if *filename == "" {
+		fmt.Fprintln(os.Stderr, "Usage of go-hrpt-decoder:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	buf := bytes.NewBuffer(nil)
 	file, err := os.Open(*filename)
@@ -82,11 +136,18 @@ func main() {
 		}
 	}
 
-	if *channel == -1 {
-		for i := 0; i < 5; i++ {
-			saveImage(i, width, height, slice16bits)
-		}
+	if *rChannel != -1 && *gChannel != -1 && *bChannel != -1 {
+		imageR := processFrame(*rChannel, width, height, slice16bits, *stretch, *southbound)
+		imageG := processFrame(*gChannel, width, height, slice16bits, *stretch, *southbound)
+		imageB := processFrame(*bChannel, width, height, slice16bits, *stretch, *southbound)
+		saveImageRGB(imageR, imageG, imageB, *rChannel, *gChannel, *bChannel)
 	} else {
-		saveImage(*channel, width, height, slice16bits)
+		if *channel == -1 {
+			for i := 0; i < 5; i++ {
+				saveImageGray(processFrame(i, width, height, slice16bits, *stretch, *southbound), i)
+			}
+		} else {
+			saveImageGray(processFrame(*channel, width, height, slice16bits, *stretch, *southbound), *channel)
+		}
 	}
 }
